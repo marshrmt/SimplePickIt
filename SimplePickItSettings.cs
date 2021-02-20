f1895d8c158e90f27c34e3f1b6e8a569d73e7142
Loloppe <54846763+Loloppe@users.noreply.github.com>
2021-02-19 22:07:47 -0500

    Update SimplePickItSettings.cs

--- SimplePickItSettings.cs
+++ SimplePickItSettings.cs
@@ -9,17 +9,29 @@ namespace SimplePickIt
     {
         public ToggleNode Enable { get; set; } = new ToggleNode(true);
 
-        [Menu("PickUp Hotkey")]
-        public HotkeyNode PickUpKey { get; set; } = new HotkeyNode(Keys.F1);
+        [Menu("MAKE SURE YOUR HOTKEY IN-GAME MATCH WITH THOSE IN HERE")]
+        public ButtonNode Text { get; set; } = new ButtonNode();
 
-        [Menu("Toggle Highlighting Hotkey")]
-        public HotkeyNode HighlightToggle { get; set; } = new HotkeyNode(Keys.Z);
+        [Menu("Item pickup key : ")]
+        public HotkeyNode PickUpKey { get; set; } = new HotkeyNode(Keys.F);
 
         [Menu("Current Movement Speed %")]
         public RangeNode<int> MovementSpeed { get; set; } = new RangeNode<int>(0, 0, 300);
         
         [Menu("Maximum Pickup Range in Unit")]
         public RangeNode<int> Range { get; set; } = new RangeNode<int>(50, 0, 100);
+
+        [Menu("Toggle highlighting : ")]
+        public HotkeyNode HighlightToggle { get; set; } = new HotkeyNode(Keys.Z);
+
+        [Menu("Number of attempt before refreshing label :")]
+        public ButtonNode Text2 { get; set; } = new ButtonNode();
+
+        [Menu("Minimum (0 = off)")]
+        public RangeNode<int> MinLoop { get; set; } = new RangeNode<int>(5, 0, 10);
+
+        [Menu("Maximum")]
+        public RangeNode<int> MaxLoop { get; set; } = new RangeNode<int>(10, 1, 10);
 
         [Menu("Consider Latency?")]
         public ToggleNode Latency { get; set; } = new ToggleNode(true);
