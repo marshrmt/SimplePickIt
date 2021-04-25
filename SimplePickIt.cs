571f89add159ccba47285a50b6940f0c1fecf664
vnz <marshrmt@gmail.com>
2021-04-25 18:31:44 +0300

    running false on any exception

--- SimplePickIt.cs
+++ SimplePickIt.cs
@@ -89,95 +89,102 @@ namespace SimplePickIt
 
         private void PickItem()
         {
-            var window = GameController.Window.GetWindowRectangle();
-            Stopwatch waitingTime = new Stopwatch();
-            int highlight = 0;
-            int limit = 0;
-            LabelOnGround nextItem = null;
-
-            var itemList = GetItemToPick();
-            if (itemList == null)
+            try
             {
+                var window = GameController.Window.GetWindowRectangle();
+                Stopwatch waitingTime = new Stopwatch();
+                int highlight = 0;
+                int limit = 0;
+                LabelOnGround nextItem = null;
+
+                var itemList = GetItemToPick();
+                if (itemList == null)
+                {
+                    IsRunning = false;
+                    return;
+                }
+
+                do
+                {
+                    if (GameController.Game.IngameState.IngameUi.InventoryPanel.IsVisible)
+                    {
+                        IsRunning = false;
+                        return;
+                    }
+
+                    if (Settings.MinLoop.Value != 0)
+                    {
+                        if (Settings.MaxLoop.Value < Settings.MinLoop.Value)
+                        {
+                            int temp = Settings.MaxLoop.Value;
+                            Settings.MaxLoop.Value = Settings.MinLoop.Value;
+                            Settings.MinLoop.Value = temp;
+                        }
+                        if (highlight == 0)
+                        {
+                            limit = Random.Next(Settings.MinLoop.Value, Settings.MaxLoop.Value + 1);
+                        }
+                        if (highlight == limit - 1)
+                        {
+                            Input.KeyDown(Settings.HighlightToggle.Value);
+                            Thread.Sleep(Random.Next(20, 25));
+                            Input.KeyUp(Settings.HighlightToggle.Value);
+                            Thread.Sleep(Random.Next(20, 25));
+                            Input.KeyDown(Settings.HighlightToggle.Value);
+                            Thread.Sleep(Random.Next(20, 25));
+                            Input.KeyUp(Settings.HighlightToggle.Value);
+                            Thread.Sleep(Random.Next(20, 25));
+                            highlight = -1;
+                        }
+                        highlight++;
+                    }
+
+                    if (itemList.Count() > 1)
+                    {
+                        itemList = itemList.Where(label => label != null).Where(label => label.ItemOnGround != null).OrderBy(label => label.ItemOnGround.DistancePlayer).ToList();
+                    }
+
+                    nextItem = itemList[0];
+
+                    if (nextItem.ItemOnGround.DistancePlayer > Settings.Range.Value)
+                    {
+                        IsRunning = false;
+                        return;
+                    }
+
+                    var centerOfLabel = nextItem?.Label?.GetClientRect().Center
+                        + window.TopLeft
+                        + new Vector2(Random.Next(0, 2), Random.Next(0, 2));
+                    if (!centerOfLabel.HasValue)
+                    {
+                        IsRunning = false;
+                        return;
+                    }
+
+                    Input.SetCursorPos(centerOfLabel.Value);
+                    Thread.Sleep(Random.Next(15, 20));
+                    Input.Click(MouseButtons.Left);
+
+                    waitingTime.Start();
+                    while (nextItem.ItemOnGround.IsTargetable && waitingTime.ElapsedMilliseconds < Settings.MaxWaitTime.Value)
+                    {
+                        ;
+                    }
+                    waitingTime.Reset();
+
+                    if (!nextItem.ItemOnGround.IsTargetable)
+                    {
+                        itemList.RemoveAt(0);
+                    }
+                } while (Input.GetKeyState(Settings.PickUpKey.Value) && itemList.Any());
+
                 IsRunning = false;
                 return;
             }
-
-            do
+            catch
             {
-                if (GameController.Game.IngameState.IngameUi.InventoryPanel.IsVisible)
-                {
-                    IsRunning = false;
-                    return;
-                }
-
-                if (Settings.MinLoop.Value != 0)
-                {
-                    if (Settings.MaxLoop.Value < Settings.MinLoop.Value)
-                    {
-                        int temp = Settings.MaxLoop.Value;
-                        Settings.MaxLoop.Value = Settings.MinLoop.Value;
-                        Settings.MinLoop.Value = temp;
-                    }
-                    if (highlight == 0)
-                    {
-                        limit = Random.Next(Settings.MinLoop.Value, Settings.MaxLoop.Value + 1);
-                    }
-                    if (highlight == limit - 1)
-                    {
-                        Input.KeyDown(Settings.HighlightToggle.Value);
-                        Thread.Sleep(Random.Next(20, 25));
-                        Input.KeyUp(Settings.HighlightToggle.Value);
-                        Thread.Sleep(Random.Next(20, 25));
-                        Input.KeyDown(Settings.HighlightToggle.Value);
-                        Thread.Sleep(Random.Next(20, 25));
-                        Input.KeyUp(Settings.HighlightToggle.Value);
-                        Thread.Sleep(Random.Next(20, 25));
-                        highlight = -1;
-                    }
-                    highlight++;
-                }
-
-                if (itemList.Count() > 1)
-                {
-                    itemList = itemList.Where(label => label != null).Where(label => label.ItemOnGround != null).OrderBy(label => label.ItemOnGround.DistancePlayer).ToList();
-                }
-
-                nextItem = itemList[0];
-
-                if (nextItem.ItemOnGround.DistancePlayer > Settings.Range.Value)
-                {
-                    IsRunning = false;
-                    return;
-                }
-
-                var centerOfLabel = nextItem?.Label?.GetClientRect().Center
-                    + window.TopLeft
-                    + new Vector2(Random.Next(0, 2), Random.Next(0, 2));
-                if (!centerOfLabel.HasValue)
-                {
-                    IsRunning = false;
-                    return;
-                }
-
-                Input.SetCursorPos(centerOfLabel.Value);
-                Thread.Sleep(Random.Next(15, 20));
-                Input.Click(MouseButtons.Left);
-
-                waitingTime.Start();
-                while (nextItem.ItemOnGround.IsTargetable && waitingTime.ElapsedMilliseconds < Settings.MaxWaitTime.Value)
-                {
-                    ;
-                }
-                waitingTime.Reset();
-
-                if(!nextItem.ItemOnGround.IsTargetable)
-                {
-                    itemList.RemoveAt(0);
-                }
-            } while (Input.GetKeyState(Settings.PickUpKey.Value) && itemList.Any());
-
-            IsRunning = false;
-            return;
+                IsRunning = false;
+            }
         }
     }
 }
