aa09e251d2cc34f81250db2b3c015e842f8a0974
vnz <marshrmt@gmail.com>
2021-10-29 00:02:25 +0300

    test

--- SimplePickIt.cs
+++ SimplePickIt.cs
@@ -88,7 +88,8 @@ namespace SimplePickIt
                         && label.ItemOnGround.Type == EntityType.WorldItem
                         && label.IsVisible
                         && label.CanPickUp
-                        && !IsUniqueItem(label))
+                        //&& !IsUniqueItem(label)
+                        )
                     .OrderBy(label => label.ItemOnGround.DistancePlayer)
                     .ToList();
             }
